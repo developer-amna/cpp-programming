7396e5b521d232ff029606053e8185cecf8f5f13
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-29 21:40:45 +0500

    practice

--- basics.cpp
+++ basics.cpp
@@ -24,3 +24,17 @@ int main()
 	cout<<"factorial="<<f;
 	return 0;
 }
+
+\\ Program # 3
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	float sum,a;
+	sum=0.0;
+	for(a=1;a<=10;a+=2)
+	sum=sum+1/a;
+	cout<<"sum of series="<<sum;
+	return 0;
+}

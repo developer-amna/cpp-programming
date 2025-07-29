a98bad91305fa1f9f73acdbbba6e88320ae0353e
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-29 21:48:35 +0500

    practice

--- basics.cpp
+++ basics.cpp
@@ -100,3 +100,20 @@ int main()
 	  }	
 	  return 0;
 }
+
+\\ Program # 8
+
+
+using namespace std;
+int main()
+{
+  for(int i=1;i<=5;i++)
+  {
+  	for(int j=1;j<=4;j++)
+  	{
+       cout<<i;
+	  }
+	  cout<<endl;
+	  }	
+	  return 0;
+}

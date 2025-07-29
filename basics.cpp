765491e5d3605c89315a764cc7c9791c9643889d
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-29 22:02:17 +0500

    loop

--- basics.cpp
+++ basics.cpp
@@ -230,3 +230,24 @@ int main()
 	while(n<=17);
 	return 0;
 }
+
+\\ Program # 15
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int s,e;
+	cout<<"Enter starting number: ";
+	cin>>s;
+	cout<<"Enter ending number: ";
+	do
+	{
+		if(s%2==0)
+		cout<<s<<"\t";
+		s++;
+	}
+	while(s<=e);
+	return 0;
+	
+}

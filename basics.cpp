923dee030d1a431338537d1b0d950cd1c9688727
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-29 22:10:10 +0500

    loop

--- basics.cpp
+++ basics.cpp
@@ -330,3 +330,22 @@ int main()
 	}
 	return 0;
 }
+
+\\ Program # 21
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int c=1;
+	for(int i=5;i<=1;i--)
+	{
+		for(int j=i;j<=1;j++)
+		{
+		cout<<j<<"\t";}
+	
+        }
+    cout<<endl;
+	}
+	  return 0;
+}

c990c6e987c16a694bc2d142384b9808f70e270f
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-29 12:22:41 +0500

    practice

--- basics.cpp
+++ basics.cpp
@@ -7,3 +7,20 @@ int main()
 	cout<<"hello world";
 	return 0;
 }
+
+
+\\ Program # 2
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,f,a;
+	cout<<"Enter a number: ";
+	cin>>n;
+	f=1;
+	for(a=1;a<=n;a++)
+	f=f*a;
+	cout<<"factorial="<<f;
+	return 0;
+}

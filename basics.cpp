c2ce961a4f52e87ec6dfc75f16ea602c50860e9c
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-29 21:45:20 +0500

    practice

--- basics.cpp
+++ basics.cpp
@@ -66,3 +66,20 @@ int main()
 	cout<<"sum of series="<<sum;
 	return 0;
 }
+
+\\ Program # 6
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int i=1;i<=5;i++)
+  {
+  	for(int j=1;j<=i;j++)
+  	{
+  	cout<<" *";
+	  }
+	  cout<<endl;
+	  }	
+	  return 0;
+}

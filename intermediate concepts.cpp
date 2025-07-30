68bc62ab5a51035f8772beee41278f98c1e33b58
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-30 22:14:36 +0500

    cpp

--- intermediate concepts.cpp
+++ intermediate concepts.cpp
@@ -26,3 +26,37 @@ int main()
 	  cout<<arr[i]<<" ";
 	  return 0;
 }
+
+\\ Program # 2
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int arr[5],i,j,temp,min;
+	for(i=0;i<5;i++)
+	{
+		cout<<"Enter value: ";
+		cin>>arr[i];
+	}
+	cout<<"The original values in array: ";
+	for(i=0;i<5;i++)
+	cout<<arr[i]<<" ";
+	
+	for(i=0;i<4;i++)
+	{
+		for(j=i+1;j<5;j++)
+		if(arr[j]<arr[min])
+		  min=j;
+	 if(min !=i)
+	 {
+	 	temp=arr[i];
+	 	arr[i]=arr[min];
+	 	arr[min]=temp;
+	 }
+	}
+	cout<<"\nThe sorted array:\n";
+	for(i=0;i<5;i++)
+	 cout<<arr[i]<<" ";
+	 return 0;
+}

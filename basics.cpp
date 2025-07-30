571282e2e875499fdb49f5efc4fd73ae18ba2844
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-30 14:45:24 +0500

    loop

--- basics.cpp
+++ basics.cpp
@@ -888,6 +888,27 @@ int main()
 	return 0;
 }
 
+\\ Program # 49
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	int x,sum=0;
+	cout<<"Enter the value of x= ";
+	cin>>x;
+	for(int i=1;i<5;i++)
+	{
+	   if(i==1)
+	      sum+=i;
+	   else
+	      sum=sum+i*pow(x,i-1);
+	}
+	cout<<"sum="<<sum;
+	return 0;
+}
+
 
 
 

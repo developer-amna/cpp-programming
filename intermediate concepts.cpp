f084de36b62e2c4ba21d85c974793abef700e3d2
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-30 22:28:17 +0500

    cpp

--- intermediate concepts.cpp
+++ intermediate concepts.cpp
@@ -171,3 +171,25 @@ int main()
    	sum=sum+arr[i];
    	cout<<sum;
    }
+
+   \\ Program # 8
+
+   a#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[10]={10,20,30,40,50,60,70,80,90,100};
+	int i, n, loc=-1;
+	cout<<"Enter value to find: ";
+	cin>>n;
+	for(i=0;i<=10;i++)
+	{
+		if(arr[i]==n)
+		loc=i;
+	}
+	if(loc==-1)
+	  cout<<"Value not found in the array";
+	else
+	  cout<<"Value found at index"<<loc;
+	  return 0;
+}

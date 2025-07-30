0227120811662e651d0f6e31eae5b6e7d29d69cc
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-30 22:18:17 +0500

    cpp

--- intermediate concepts.cpp
+++ intermediate concepts.cpp
@@ -82,3 +82,21 @@ int main()
 	  }
 	  return 0;
 }
+
+\\ Program # 4
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5] , i;
+	for(i=0;i<5;i++)
+	{
+		cout<<"Enter an integers: ";
+		cin>>arr[i];
+	}
+	 cout<<"The values in array are :\n"<<endl;
+	 for(i=0;i<5;i++)
+	 cout<<arr[i]<<endl;
+	 return 0;
+}

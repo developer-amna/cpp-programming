8b8485d33dc468fbe54615497f213182f92a521c
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-30 14:44:15 +0500

    loop

--- basics.cpp
+++ basics.cpp
@@ -869,6 +869,25 @@ int main()
 	return 0;
 }
 
+\\ Program # 48
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int fact=1,sum=0;
+	{
+	for(int i=1;i<=5;i++)
+	    {
+		for(int j=1;j<=i;j++)
+		fact=fact*j;
+	    }
+	    sum=sum+fact;
+	}
+	cout<<"sum of series="<<sum;
+	return 0;
+}
+
 
 
 

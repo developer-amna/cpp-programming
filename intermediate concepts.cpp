816efc400b87d5254bfe4e06d17c454abaf4c5bb
developer Amna <hafizaamna.webdev@gmail.com>
2025-07-30 22:25:03 +0500

    cpp

--- intermediate concepts.cpp
+++ intermediate concepts.cpp
@@ -119,3 +119,36 @@ int main()
 	cout<<"The number of days in this year till date="<<total<<endl;
 	return 0;
 }
+
+\\ Program # 6
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[10]={10,20,30,40,50,60,70,80,90,100};
+	int n,start,mid,end,loc;
+	start=0;
+	end=9;
+	loc=-1;
+	cout<<"Enter number to find: ";
+	cin>>n;
+	while(start<=end)
+	{
+		mid=(start+end)/2;
+		if(arr[mid]==n)
+		{
+		loc=mid;
+		break;	
+		}
+		else if(n<arr[mid])
+		  end=mid-1;
+		else
+		  start=mid+1;
+	}
+	if(loc==-1)
+	  cout<<n<<"not found!";
+	else
+	  cout<<n<<"found at index."<<loc;
+	  return 0;
+}

067123498b58afd7d204f68d554ff8174b35ee52
developer Amna <hafizaamna.webdev@gmail.com>
2025-08-01 22:20:29 +0500

    cpp

--- intermediate concepts.cpp
+++ intermediate concepts.cpp
@@ -522,7 +522,21 @@ int main(){
     }
 }
 
+\\ Program # 15
 
+#include<iostream>
+using namespace std;
+int add(int *a, int *b);
+int main()
+{
+	int x=2, y=3;
+ cout<<add(&x,&y);
+	return 0;
+}
+ add( int *a, int *b)
+{
+	return (*a + *b);
+}
 
 
 

78d033ca11388ec08b97a79f05cf4040ad407ce9
developer Amna <hafizaamna.webdev@gmail.com>
2025-10-22 12:31:53 +0500

    smallest in array

--- basics.cpp
+++ basics.cpp
@@ -1902,6 +1902,28 @@ int main(){
 	return 0;
 }
 
+\\ smallest/largest in array
+
+\\ Program # 87
+
+#include<iostream>
+using namespace std;
+int main(){
+	int nums[5]={5, 15, 22, 1, -15};
+	int size = 6;
+	
+	int smallest = INT_MAX;
+	
+	for(int i=0;i<size;i++)
+	{
+		if(nums[i] < smallest) {
+			smallest = nums[i];
+		}
+	}
+	cout << "Smallest= "<<smallest<<endl;
+	return 0;
+}
+
 
 
 
